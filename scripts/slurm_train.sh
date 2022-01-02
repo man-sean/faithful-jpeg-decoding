baf735cb52fa12642325d0dff90e09fcac7b1fbd
Sean Man <sean8100@gmail.com>
2022-01-02 15:40:03 +0200

    Add slurm scripy

---
+++ scripts/slurm_train.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+CPUS=$1
+GPUS=$2
+CONFIG=$3
+#PORT=${PORT:-4321}
+#
+## usage
+#if [ $# -lt 3 ] ;then
+#    echo "usage:"
+#    echo "./scripts/slurm_train.sh [number of gpu] [path to option file]"
+#    exit
+#fi
+#
+#PYTHONPATH="$(dirname $0)/..:${PYTHONPATH}" \
+#python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+#    basicsr/train.py -opt $CONFIG --launcher pytorch ${@:3}
+
+PYTHONPATH="./:${PYTHONPATH}" \
+GLOG_vmodule=MemcachedClient=-1 \
+srun -p cactus -A cactus --mpi=pmi2 --job-name=JPEG --gres=gpu:$GPUS --ntasks=1 --ntasks-per-node=1 --cpus-per-task=$CPUS --kill-on-bad-exit=1 \
+python -u basicsr/train.py -opt $CONFIG --launcher="slurm"
