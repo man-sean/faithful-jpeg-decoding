157616cf010e47cfcc9ffc9273cb53fb76f8cfb5
Sean Man <sean8100@gmail.com>
2022-01-04 10:31:41 +0200

    Fix launcher bug

--- scripts/slurm_train.sh
+++ scripts/slurm_train.sh
@@ -16,7 +16,14 @@ CONFIG=$3
 #python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
 #    basicsr/train.py -opt $CONFIG --launcher pytorch ${@:3}
 
+#case "$DEBUG_CUDA" in
+#    ON) CUDA_LAUNCH_BLOCKING=1 ;;
+#    *) CUDA_LAUNCH_BLOCKING=0 ;;
+#esac
+
 PYTHONPATH="./:${PYTHONPATH}" \
 GLOG_vmodule=MemcachedClient=-1 \
+#CUDA_LAUNCH_BLOCKING=1 \ # for debugging
+#[ "$DEBUG_CUDA" == "ON" ] && CUDA_LAUNCH_BLOCKING=1 \ || CUDA_LAUNCH_BLOCKING=0 \
 srun -p cactus -A cactus --mpi=pmi2 --job-name=JPEG --gres=gpu:$GPUS --ntasks=1 --ntasks-per-node=1 --cpus-per-task=$CPUS --kill-on-bad-exit=1 \
-python -u basicsr/train.py -opt $CONFIG --launcher="slurm"
+python -u basicsr/train.py -opt $CONFIG #--launcher="slurm"
