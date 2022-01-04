f97e45215e21172608f862416de4ba233a944248
Sean Man <sean8100@gmail.com>
2022-01-04 10:25:11 +0200

    Add faithfulness and FID metrics

--- basicsr/metrics/__init__.py
+++ basicsr/metrics/__init__.py
@@ -2,9 +2,11 @@ from copy import deepcopy
 
 from basicsr.utils.registry import METRIC_REGISTRY
 from .niqe import calculate_niqe
-from .psnr_ssim import calculate_psnr, calculate_ssim
+from .psnr_ssim import calculate_psnr, calculate_ssim, calculate_sr_faithfulness_rmse, calculate_jpeg_faithfulness_rmse
+from .fid import calculate_fid_given_paths
 
-__all__ = ['calculate_psnr', 'calculate_ssim', 'calculate_niqe']
+__all__ = ['calculate_psnr', 'calculate_ssim', 'calculate_niqe',
+           'calculate_sr_faithfulness_rmse', 'calculate_jpeg_faithfulness_rmse', 'calculate_fid_given_paths']
 
 
 def calculate_metric(data, opt):
--- basicsr/metrics/psnr_ssim.py
+++ basicsr/metrics/psnr_ssim.py
@@ -3,6 +3,51 @@ import numpy as np
 
 from basicsr.metrics.metric_util import reorder_image, to_y_channel
 from basicsr.utils.registry import METRIC_REGISTRY
+from basicsr.utils.matlab_functions import imresize
+from functools import partial
+from torch.nn.functional import interpolate
+import torch
+from basicsr.utils.img_util import img2tensor, tensor2img
+from basicsr.utils.diffjpeg import DiffJPEG
+
+
+@METRIC_REGISTRY.register()
+def calculate_rmse(img, img2, crop_border, input_order='HWC', test_y_channel=False, **kwargs):
+    """Calculate PSNR (Peak Signal-to-Noise Ratio).
+
+    Ref: https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio
+
+    Args:
+        img (ndarray): Images with range [0, 255].
+        img2 (ndarray): Images with range [0, 255].
+        crop_border (int): Cropped pixels in each edge of an image. These
+            pixels are not involved in the PSNR calculation.
+        input_order (str): Whether the input order is 'HWC' or 'CHW'.
+            Default: 'HWC'.
+        test_y_channel (bool): Test on Y channel of YCbCr. Default: False.
+
+    Returns:
+        float: psnr result.
+    """
+
+    assert img.shape == img2.shape, (f'Image shapes are different: {img.shape}, {img2.shape}.')
+    if input_order not in ['HWC', 'CHW']:
+        raise ValueError(f'Wrong input_order {input_order}. Supported input_orders are ' '"HWC" and "CHW"')
+    img = reorder_image(img, input_order=input_order)
+    img2 = reorder_image(img2, input_order=input_order)
+    img = img.astype(np.float64)
+    img2 = img2.astype(np.float64)
+
+    if crop_border != 0:
+        img = img[crop_border:-crop_border, crop_border:-crop_border, ...]
+        img2 = img2[crop_border:-crop_border, crop_border:-crop_border, ...]
+
+    if test_y_channel:
+        img = to_y_channel(img)
+        img2 = to_y_channel(img2)
+
+    mse = np.mean((img - img2)**2)
+    return np.sqrt(mse)
 
 
 @METRIC_REGISTRY.register()
@@ -44,6 +89,97 @@ def calculate_psnr(img, img2, crop_border, input_order='HWC', test_y_channel=Fal
     if mse == 0:
         return float('inf')
     return 20. * np.log10(255. / np.sqrt(mse))
+
+
+# TODO: update the description of this function.
+# TODO: can we support more than single qf? do we need to?
+@METRIC_REGISTRY.register()
+def calculate_jpeg_faithfulness_rmse(img, gt_lq, crop_border, qf, input_order='HWC', test_y_channel=False, **kwargs):
+    """Calculate JPEG Faithfulness RMSE (Root Mean Square Error).
+
+    Args:
+        img (ndarray): Images with range [0, 255].
+        img2 (ndarray): Images with range [0, 255].
+        crop_border (int): Cropped pixels in each edge of an image. These
+            pixels are not involved in the PSNR calculation.
+        input_order (str): Whether the input order is 'HWC' or 'CHW'.
+            Default: 'HWC'.
+        test_y_channel (bool): Test on Y channel of YCbCr. Default: False.
+
+    Returns:
+        float: psnr result.
+    """
+    jpeger = DiffJPEG(differentiable=True)
+    result_lq = tensor2img(jpeger(img2tensor(img / 255.).unsqueeze(0), quality=qf).squeeze(0))
+    assert result_lq.shape == gt_lq.shape, (f'LQ image shapes are different: {result_lq.shape}, {gt_lq.shape}.')
+    if input_order not in ['HWC', 'CHW']:
+        raise ValueError(f'Wrong input_order {input_order}. Supported input_orders are ' '"HWC" and "CHW"')
+    result_lq = reorder_image(result_lq, input_order=input_order)
+    gt_lq = reorder_image(gt_lq, input_order=input_order)
+    img = result_lq.astype(np.float64)
+    img2 = gt_lq.astype(np.float64)
+
+    if crop_border != 0:
+        img = img[crop_border:-crop_border, crop_border:-crop_border, ...]
+        img2 = img2[crop_border:-crop_border, crop_border:-crop_border, ...]
+
+    if test_y_channel:
+        img = to_y_channel(img)
+        img2 = to_y_channel(img2)
+
+    mse = np.mean((img - img2)**2)
+    return np.sqrt(mse)
+
+
+# TODO: update the description of this function.
+@METRIC_REGISTRY.register()
+def calculate_sr_faithfulness_rmse(img, gt_lq, crop_border, scale, input_order='HWC', test_y_channel=False,
+                                   imresize_backend='matlab', imresize_mode='cubic', **kwargs):
+    """Calculate SR Faithfulness RMSE (Root Mean Square Error).
+
+    Args:
+        img (ndarray): Images with range [0, 255].
+        img2 (ndarray): Images with range [0, 255].
+        crop_border (int): Cropped pixels in each edge of an image. These
+            pixels are not involved in the PSNR calculation.
+        input_order (str): Whether the input order is 'HWC' or 'CHW'.
+            Default: 'HWC'.
+        test_y_channel (bool): Test on Y channel of YCbCr. Default: False.
+
+    Returns:
+        float: psnr result.
+    """
+    if imresize_backend == 'matlab':
+        assert imresize_mode == 'cubic'
+        imresize_func = partial(imresize, scale=1.0 / scale, antialiasing=True)
+    elif imresize_backend == 'torch':
+        def torch_imresize_pipeline(img, scale_factor, mode):
+            img = img2tensor(img / 255.0).unsqueeze(0)
+            img = interpolate(img, scale_factor=scale_factor, mode=mode)
+            img = tensor2img(img.squeeze(0))
+            return img
+        imresize_func = partial(torch_imresize_pipeline, scale_factor=1.0 / scale, mode=imresize_mode)
+    else:
+        raise NotImplementedError()
+    result_lq = imresize_func(img)
+    assert result_lq.shape == gt_lq.shape, (f'LQ image shapes are different: {result_lq.shape}, {gt_lq.shape}.')
+    if input_order not in ['HWC', 'CHW']:
+        raise ValueError(f'Wrong input_order {input_order}. Supported input_orders are ' '"HWC" and "CHW"')
+    result_lq = reorder_image(result_lq, input_order=input_order)
+    gt_lq = reorder_image(gt_lq, input_order=input_order)
+    img = result_lq.astype(np.float64)
+    img2 = gt_lq.astype(np.float64)
+
+    if crop_border != 0:
+        img = img[crop_border:-crop_border, crop_border:-crop_border, ...]
+        img2 = img2[crop_border:-crop_border, crop_border:-crop_border, ...]
+
+    if test_y_channel:
+        img = to_y_channel(img)
+        img2 = to_y_channel(img2)
+
+    mse = np.mean((img - img2)**2)
+    return np.sqrt(mse)
 
 
 def _ssim(img, img2):
